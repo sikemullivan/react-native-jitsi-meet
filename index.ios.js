ff5a89a629a6c414617c8b516387bf6a10d6559d
Michael Sullivan <michael.sullivan@rusull.com>
2020-06-10 20:49:46 -0500

    updated js

--- index.ios.js
+++ index.ios.js
@@ -7,10 +7,15 @@ import { NativeModules, requireNativeComponent } from 'react-native';
 export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
 export const JitsiMeetModule = NativeModules.RNJitsiMeetView;
 const call = JitsiMeetModule.call;
+const callToken = JitsiMeetModule.callToken;
 const audioCall = JitsiMeetModule.audioCall;
 JitsiMeetModule.call = (url, userInfo) => {
   userInfo = userInfo || {};
   call(url, userInfo);
+}
+JitsiMeetModule.callToken = (url, userInfo, token) => {
+  userInfo = userInfo || {};
+  callToken(url, userInfo, token);
 }
 JitsiMeetModule.audioCall = (url, userInfo) => {
   userInfo = userInfo || {};
